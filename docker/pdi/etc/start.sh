4254f174eed66cf02d0b8526954f4937e85bbaab
Edrisse <edrisseasm@gmail.com>
2019-05-14 11:18:51 +0200

    start.sh waits for database to be ready

--- docker/pdi/etc/start.sh
+++ docker/pdi/etc/start.sh
@@ -1,4 +1,12 @@
 echo "Creating database"
-mysql -u$MERGE_TOOL_DB_USER -pAdmin123 -h$MERGE_TOOL_DB_HOST -e "create database merda;"
+
+mysql -u$MERGE_TOOL_DB_USER -pAdmin123 -h$MERGE_TOOL_DB_HOST -e "select 1;"
+echo $?
+until [ $? -eq 0 ]
+do
+  sleep 1
+  mysql -u$MERGE_TOOL_DB_USER -pAdmin123 -h$MERGE_TOOL_DB_HOST -e "select 1;"
+done
+
 echo "Merge Tool PDI Container"
 while true; do sleep 1000; done
